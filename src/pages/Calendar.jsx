72cc3cfef3e6ad0dc50b6789b1669133ab892d1f
Eunju Lee <gleunju217@gmail.com>
2024-05-30 10:40:25 +0900

    240530 lej : calendar custom

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,16 +1,51 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid';
-
+import '../styles/calendar.css'
 
 
 export default function Calendar() {
+  const renderDayCellContent = (info) => {
+    let number = document.createElement("a");
+    number.classList.add("fc-daygrid-day-number");
+    number.innerHTML = info.dayNumberText.replace("일", "");
+    if (info.view.type === "dayGridMonth") {
+      return { html: number.outerHTML };
+    }
+    return {
+      domNodes: []
+    };
+  };
+  
+  
+
   return (
     <div>
       <FullCalendar 
           defaultView="dayGridMonth" 
           plugins={[ dayGridPlugin ]}
+          height="auto"
+          locale="ko"
+          dayCellContent={renderDayCellContent}
         />
+
+      <div className = "plan-wrapper">
+        <div>Working Planning</div>
+        <button className ="plan-box-btn">
+          <div className ="plan-plus-wrapper">+</div>
+        </button>
+      </div>
+
+
+      <div className = "plan-wrapper">
+        <div>Travel Planning</div>
+        <button className ="plan-box-btn" style={{border:"2px solid #B2DEFC"}}>
+          <div className ="plan-plus-wrapper">+</div>
+        </button>
+      </div>
+
+
+
     </div>
   )
 }
